d723ec608347e0c22ac91a4adbba617a82e5ef60
Endra <endrawahyudibs@gmail.com>
2021-10-27 09:16:16 +0700

    update

--- SINAU KODING/Backend Programming #16/project/perpustakaan/src/test/java/com/sinaukoding/perpustakaan/entity/BaseEntityTest.java
+++
@@ -1,7 +0,0 @@
-package com.sinaukoding.perpustakaan.entity;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class BaseEntityTest {
-
-}
---
+++ SinauKoding/BackendProgramming#16/project/perpustakaan/src/main/java/com/sinaukoding/perpustakaan/entity/Book.java
@@ -0,0 +1,36 @@
+package com.sinaukoding.perpustakaan.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "book")
+@Setter
+@Getter
+@NoArgsConstructor
+
+public class Book extends BaseEntity<Book> {
+
+
+    private static final long serialVersionUID = -5223709546195523515L;
+    @Column(name = "title", columnDefinition = "VARCHAR(50)")
+    private String tittle;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "description")
+    private String description;
+
+}
