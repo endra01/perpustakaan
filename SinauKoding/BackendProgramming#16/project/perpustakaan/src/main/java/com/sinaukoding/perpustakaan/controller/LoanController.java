b7ac216e4fdaed14280fd4bcc4610cf0f6897482
Endra <endrawahyudibs@gmail.com>
2021-11-01 01:12:48 +0700

    push loan

--- SinauKoding/BackendProgramming#16/project/perpustakaan/src/main/java/com/sinaukoding/perpustakaan/controller/LoanController.java
+++ SinauKoding/BackendProgramming#16/project/perpustakaan/src/main/java/com/sinaukoding/perpustakaan/controller/LoanController.java
@@ -6,6 +6,7 @@ import com.sinaukoding.perpustakaan.common.RestResult;
 import com.sinaukoding.perpustakaan.common.StatusCode;
 import com.sinaukoding.perpustakaan.entity.Loan;
 import com.sinaukoding.perpustakaan.service.LoanService;
+import com.sinaukoding.perpustakaan.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("loans")
 public class LoanController extends BaseController {
+
     @Autowired
     private LoanService service;
 
@@ -30,6 +32,23 @@ public class LoanController extends BaseController {
         Long rows = service.count(loan);
         return new RestResult(rows > 0 ? service.find(loan, offset, limit) : new ArrayList<>(), rows);
     }
+
+    /*func find bydate*/
+    @PreAuthorize("permitAll()")
+    @GetMapping(value = "by-date")
+    public RestResult findByDate(@RequestParam(value = "param", required = false) String param,
+                                @RequestParam(value = "start-date") String startDate,
+                                @RequestParam(value = "end-date") String endDate) throws JsonProcessingException {
+
+        RestResult result = new RestResult(StatusCode.OPERATION_FAILED);
+        Loan loan = param != null ? new ObjectMapper().readValue(param, Loan.class) : new Loan();
+
+        result.setData(service.findByDate(loan, DateUtils.fromString(startDate), DateUtils.fromString(endDate)));
+        result.setRows((long) service.findByDate(loan, DateUtils.fromString(startDate), DateUtils.fromString(endDate)).size());
+
+        return result;
+    }
+
 
     @PreAuthorize("permitAll()")
     @PostMapping
--- SinauKoding/BackendProgramming#16/project/perpustakaan/src/main/java/com/sinaukoding/perpustakaan/dao/LoanDAO.java
+++ SinauKoding/BackendProgramming#16/project/perpustakaan/src/main/java/com/sinaukoding/perpustakaan/dao/LoanDAO.java
@@ -3,9 +3,9 @@ package com.sinaukoding.perpustakaan.dao;
 import com.sinaukoding.perpustakaan.entity.Loan;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -15,6 +15,32 @@ public class LoanDAO extends BaseDAO<Loan> {
     public List<Predicate> predicates(Loan param, CriteriaBuilder builder, Root<Loan> root, boolean isCount) {
         List<Predicate> predicates = super.predicates(param, builder, root, isCount);
 
+        if (!isCount){
+            root.fetch("book", JoinType.INNER);
+        }
         return predicates;
+    }
+
+        public List<Loan> findByDate(Loan param, Date startDate, Date endDate){
+            CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<Loan> query = builder.createQuery(Loan.class);
+            Root<Loan> root = query.from(Loan.class);
+
+            if (param != null){
+                if (param.getUser() != null){
+                    query.where(builder.equal(root.get("user").get("id"), param.getUser().getId()));
+                }
+            }
+
+            query.where(builder.between(root.get("loanDate"), startDate, endDate));
+            query.orderBy(builder.asc(root.get("id")));
+
+            root.fetch("book", JoinType.INNER);
+            /*root.fetch("user", JoinType.INNER);*/
+            root.fetch("user", JoinType.LEFT);/*ketika tabel loan belum ada data user, untuk menampilkan data*/
+
+            TypedQuery<Loan> typedQuery = entityManager.createQuery(query);
+
+            return typedQuery.getResultList();
     }
 }
